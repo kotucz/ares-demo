7db94f5e07d4231a1efb2e7a2577affeecdb5dba
Tomas Kotula <tomas.kotula@cleverlance.com>
2018-03-06 19:54:56 +0100

    Create new project

---
+++ app/src/main/java/kotu/cz/ares/MainActivity.kt
@@ -0,0 +1,11 @@
+package kotu.cz.ares
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
